be9fe311177055be1e98aaf506e97222082d573e
Krasmir Koeff <kr@koeff.com>
2019-04-17 13:38:24 +0300

    added console log to display state

--- src/index.js
+++ src/index.js
@@ -7,10 +7,12 @@ import * as serviceWorker from "./serviceWorker";
 import { createStore } from "redux";
 
 function reducer() {
-  return "State";
+  return "State123";
 }
 
 const store = createStore(reducer);
+
+console.log(store.getState());
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
